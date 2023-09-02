0f0381a20fb9d9f4d265b6548312dc5a5a25e09b
shashannkd <shashannkd@gmail.com>
2023-09-02 23:57:22 +0530

    initial commit

---
+++ src/main/java/com/shashannkd/movieshub/MovieshubApplication.java
@@ -0,0 +1,13 @@
+package com.shashannkd.movieshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieshubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashannkd/movieshub/MovieshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashannkd.movieshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
